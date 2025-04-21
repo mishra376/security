c74007387546a6c04e1f28a50562bfcffed0271d
Amber Mishra <mishraamber376@gmail.com>
2025-04-21 18:27:24 +0530

    Initial Commit

---
+++ src/main/java/com/in28min/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.in28min.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
